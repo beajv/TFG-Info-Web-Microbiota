aa3de52c110f7dd329179235d8806d05d83b8e57
Beatriz <beatrizjv@correo.ugr.es>
2025-06-10 20:15:08 +0200

    Prueba test

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -1,9 +1,9 @@
-# tests/test_api.py
+# backend/tests/test_api.py
 from fastapi.testclient import TestClient
-from myapp.main import app  # Cambia esto si tu app tiene otro nombre
+from myapp.main_test import app
 
 client = TestClient(app)
 
-def test_homepage():
+def test_root():
     response = client.get("/")
     assert response.status_code == 200
